77b642710a7444881e8e5da0921fd2da02ce6144
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-24 23:15:32 +0100

    fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,40 @@
-let mulit = (factor1, factor2) => {
-    
+let multi = () => {
+    let factor1 = Number(document.querySelector("#factor1").value)
+    let factor2 = Number(document.querySelector("#factor2").value)
+    let output = document.querySelector("#output")
+    let ergebnis = 0
+    // console.log(typeof(factor1));
 
-    for (let i = 0; i <= factor2; i++) {
-        (factor1 + factor2
+    for (let i = 1; i <= Math.abs(factor2); i++) {
+        
+        if (factor1 == 0 || factor2 == 0) {
+            ergebnis = 0
+         } 
+        else if (factor1 < 0) {
+            ergebnis += factor1
+        } 
+        else if (factor2 < 5) {
+            ergebnis += -factor1
+        }
+        else if (factor1 < 0 && factor2 < 0) {
+            ergebnis += factor1
+        }
+        else if (factor1 > 0 && factor2 > 0) {
+            ergebnis += factor1
+        }
+        
+        
+        
     }
+    output.innerHTML = ergebnis
 }
+
+multi()
+
+
+
+
+
+
+
+
